8b134e7afd31932409f1a8cf36da500cf3f7f3cd
Jaycop-AFK <82388750+Jaycop-AFK@users.noreply.github.com>
2025-02-27 14:32:22 +0700

    sdadsa

--- components/FacebookMsg.tsx
+++ components/FacebookMsg.tsx
@@ -4,7 +4,7 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 
 const FacebookMsg = () => {
   return (
-    <FacebookProvider appId="1195398152594629" chatSupport>
+    <FacebookProvider appId="1832413670927392" chatSupport>
       <CustomChat pageId="538960199305317" minimized={false} />
     </FacebookProvider>
   );
